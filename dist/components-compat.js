201c370b359ad9024b2a6e99ceb93007ef951b16
William Wallace <willtwall@googlemail.com>
2018-07-18 14:13:42 -0600

    Added logo for web components.

--- dist/components-compat.js
+++ dist/components-compat.js
@@ -1488,28 +1488,18 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
                 _get(ByuResources.prototype.__proto__ || Object.getPrototypeOf(ByuResources.prototype), 'connectedCallback', this).call(this);
                 util.applyTemplate(this, 'byu-resources', template, function () {
+
+                    //Web Components
                     var element = _this6.shadowRoot.querySelector('#resources');
-                    // this.shadowRoot.querySelector('#resources').innerHTML = this.info;
                     jQuery.getJSON('https://cdn.byu.edu/manifest.json', function (data) {
                         var html = '';
                         jQuery.each(data.libraries, function (name, library) {
                             if (library.type == 'web-component') {
-                                html += '<byu-feature-card class="gray-title"><div slot="title">' + library.name + '</div><div slot="feature-center">' + library.description + '</div></byu-feature-card>';
+                                html += '<byu-feature-card class="gray-title"><div slot="title">' + library.name + '</div><div slot="feature-left"><img src="./modules/custom/byu_resources/logos/html5-200px.png"></div><div slot="feature-center">' + library.description + '</div></byu-feature-card>';
                             }
                         });
                         element.innerHTML = html;
                     });
-                    // (function($) {
-                    //   $.getJSON('https://cdn.byu.edu/manifest.json', function(data) {
-                    //     $.each(data.libraries, function(name, library) {
-                    //       if (library.type == 'web-component') {
-                    //         let libraryInfo = library.name + ' ' + library.description + ' | ';
-                    //         console.log(libraryInfo);
-                    //       }
-                    //     });
-                    //
-                    //   });
-                    // })(jQuery);
                 });
 
                 // $.getJSON('https://www.drupal.org/brigham-young-university', function(data) {console.log(data)});
